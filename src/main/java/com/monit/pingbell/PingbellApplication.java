cd8da7c9d1d1d32b90ed9bd6d8d82b9948c35bca
jgm0327 <gmjang6349@gmail.com>
2026-05-21 14:21:55 +0900

    Init: 초기 세팅

---
+++ src/main/java/com/monit/pingbell/PingbellApplication.java
@@ -0,0 +1,13 @@
+package com.monit.pingbell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingbellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingbellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monit/pingbell/PingbellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monit.pingbell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingbellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
