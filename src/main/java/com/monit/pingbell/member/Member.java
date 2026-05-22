118bfed1e74c380bed5133e3ec3a3ac7ae4d443e
jgm0327 <gmjang6349@gmail.com>
2026-05-22 21:49:23 +0900

    feat(auth): add JWT-based authentication flow

---
+++ src/main/java/com/monit/pingbell/member/Member.java
@@ -0,0 +1,27 @@
+package com.monit.pingbell.member;
+
+import com.monit.pingbell.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
