5dfcc3c38aa991206e62fea285fe2c9c0c61217f
jgm0327 <gmjang6349@gmail.com>
2026-06-17 17:17:34 +0900

    FIX #7: Asia/Seoul로 시간 기준 맞춰서 스케줄러 오류 수정

--- src/main/java/com/monit/pingbell/global/config/TimeConfig.java
+++ src/main/java/com/monit/pingbell/global/config/TimeConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 @Configuration
 public class TimeConfig {
     @Bean
     public Clock clock() {
-        return Clock.systemUTC();
+        return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 }
