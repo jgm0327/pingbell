4d176b0a52f76162e79db19136970921bf6d5be3
jgm0327 <gmjang6349@gmail.com>
2026-07-14 15:10:37 +0900

    FEAT #49: 로그 분석 오류 코드와 공통 예외 처리 추가

---
+++ src/main/java/com/monit/pingbell/loganalysis/exception/LogAnalysisException.java
@@ -0,0 +1,23 @@
+package com.monit.pingbell.loganalysis.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class LogAnalysisException extends RuntimeException {
+
+    private final HttpStatus status;
+    private final String code;
+
+    public LogAnalysisException(HttpStatus status, String code, String message) {
+        super(message);
+        this.status = status;
+        this.code = code;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
